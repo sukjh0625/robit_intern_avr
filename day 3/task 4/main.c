9b3f2c8f2ea1ecbfe649c8ba3940c6a8bafee399
sukjh0625 <sukjh0625@gmail.com>
2026-08-03 02:00:27 +0900

    Update main.c

--- day 3/task 4/main.c
+++ day 3/task 4/main.c
@@ -4,52 +4,63 @@
 #include <avr/interrupt.h>
 #include <stdint.h>
 
-#define TX_BIT PD3 
-//pd3으로 데이터 보낼 핀 설정
-#define BIT_TICKS 104
-//비트시간 설정
-void wait_bit(void)//비트 하나를 보내는 시간만큼 기다리는 함수이다
+#define TX_BIT PD3
+// PD3으로 데이터를 보낼 핀 설정
+#define BIT_TICKS 208
+//9600 통신에서 비트하나를 유지해야하는 시간이 104인데 만들기 위해 설정한 값
+
+void wait_bit(void) // 비트 하나를 보내는 시간만큼 기다리는 함수이다
 {
     TCNT1 = 0;
 
     while (TCNT1 < BIT_TICKS)
     {}
+    // 타이머 값이 208이 될 때까지 기다린다
 }
 
-void send_byte(uint8_t data)//8비트를 보내는 함수
+void send_byte(uint8_t data)
+// 8비트 데이터를 보내는 함수
 {
     uint8_t a;
+    // 비트 번호를 확인할 반복 변수
 
-    PORTD &= ~(1 << TX_BIT);//tx핀이 하이 high 상태// 시작 후 low 상태로 유지
+    PORTD &= ~(1 << TX_BIT);
+    // 시작 비트는 LOW 상태로 보낸다
+
     wait_bit();
 
-    for (a = 0; a < 8; a++)//낮은 비트부터 보
+    for (a = 0; a < 8; a++)
+    // 낮은 비트부터 8개 비트를 하나씩 보낸다
     {
         if (data & (1 << a))
         {
-            PORTD |= (1 << TX_BIT);
+            PORTD |= (1 << TX_BIT); // 비트가 1이면 HIGH 전송
         }
         else
         {
-            PORTD &= ~(1 << TX_BIT);
+            PORTD &= ~(1 << TX_BIT); // 비트가 0이면 LOW 전송
         }
 
         wait_bit();
     }
 
-    PORTD |= (1 << TX_BIT);//8비트 다보낸 뒤에 정지비트 보내기 정지 비트는 high이다
+    PORTD |= (1 << TX_BIT);//정지비트 high 상태
+
     wait_bit();
 }
 
 int main(void)
 {
-    cli();//인터럽트 끄는함수
+    cli();// 통신 중 시간 오차가 생기지 않도록 인터럽트 끄게하기
 
-    DDRD |= (1 << TX_BIT);//입출력 설정
-    PORTD |= (1 << TX_BIT);
+    DDRD |= (1 << TX_BIT);
 
-    TCCR1A = 0x00;//일반 터이머 방식으로 사용
+    PORTD |= (1 << TX_BIT); // 통신하지 않을 때 HIGH 상태
+
+    TCCR1A = 0x00;
+
     TCCR1B = (1 << CS11);
+    // Timer1을 더느리게 설
 
     while (1)
     {
@@ -65,12 +76,14 @@ int main(void)
         send_byte(0x64);  // d
         send_byte(0x21);  // !
 
-        send_byte(0x0D);// 줄바
+        send_byte(0x0D);
+
         send_byte(0x0A);
 
         for (uint16_t a = 0; a < 1000; a++)
         {
             wait_bit();
         }
+        // HelloWorld!를 보낸 뒤 약 0.1초 기다린다
     }
 }
